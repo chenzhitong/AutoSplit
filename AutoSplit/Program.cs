8553b456d363f9914dce803397faf38e8596d00d
陈志同 <chenzhitong12@hotmail.com>
2018-01-12 10:42:26 +0800

    initial commit

---
+++ AutoSplit/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初始金额
+            var initConfirmd = Tools.GetBalance();
+            if(initConfirmd == 0)
+            {
+                Console.WriteLine("余额不足");
+                Console.ReadLine();
+                return;
+            }
+            //分10批转账
+            for (int i = 1; i <= 10; i++)
+            {
+                //实际金额
+                var confirmd = Tools.GetBalance();
+                //每次转账金额
+                var value = initConfirmd / (decimal)Math.Pow(2, i);
+                for (int j = 1; j <= Math.Pow(2, i); j++)
+                {
+                    if (Tools.Send("AcdB……钱包地址", value))
+                    {
+                        Console.WriteLine($"{i} 转账成功：{value}\t{j}");
+                    }
+                }
+                confirmd = Tools.GetBalance();
+                while (confirmd < initConfirmd)
+                {
+                    Thread.Sleep(3000);
+                    confirmd = Tools.GetBalance();
+                    Console.WriteLine($"{i} 已确认{confirmd}/{initConfirmd}");
+                }
+                Console.WriteLine($"第{i}批转账结束");
+            }
+            Console.WriteLine($"测试结束");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AutoSplit/Tools.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace AutoSplit
+{
+    public static class Tools
+    {
+        public static string HttpPost(string Url, string postData, int timeOut = 60000)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(Url);
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                    request.ContentLength = byteArray.Length;
+                    request.Timeout = timeOut;
+                    Stream dataStream = request.GetRequestStream();
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                    WebResponse response = request.GetResponse();
+                    dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+                    return responseFromServer;
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("网络超时，重试中……");
+                    continue;
+                }
+            }
+            return "";
+        }
+
+        public static decimal GetBalance()
+        {
+            var json = HttpPost("http://localhost:20332", "{'jsonrpc': '2.0','method': 'getbalance','params': ['602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'],'id': 1}");
+            return (decimal)JObject.Parse(json)["result"]["confirmed"];
+        }
+
+        public static bool Send(string to, decimal value)
+        {
+            var json = HttpPost("http://localhost:20332", $"{{'jsonrpc': '2.0','method': 'sendtoaddress','params': ['602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7','{to}',{value}],'id': 1}}");
+            var a = JObject.Parse(json)["result"];
+            return a != null;
+        }
+    }
+
+}
