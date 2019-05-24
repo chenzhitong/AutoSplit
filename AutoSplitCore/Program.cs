779f665f27b2d8950466e00c2d46ea3b91573467
chenzhitong <chenzhitong12@qq.com>
2019-05-24 15:56:33 +0800

    upgrade to .net core

---
+++ AutoSplitCore/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace AutoSplitCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = new string[0];
+            try
+            {
+                address = File.ReadAllLines("address.txt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("请创建 address.txt 文件，每一行为钱包中的一个地址，支持多个地址。仅有一个地址时文件的内容就是地址本身");
+                Console.ReadLine();
+            }
+            var random = new Random();
+
+            //初始金额
+            var initConfirmd = Tools.GetBalance();
+            if (initConfirmd == 0)
+            {
+                Console.WriteLine("余额不足");
+                Console.ReadLine();
+                return;
+            }
+            //分10批转账
+            for (int i = 1; i <= 10; i++)
+            {
+                //实际金额
+                var confirmd = Tools.GetBalance();
+                //每次转账金额
+                var value = (int)(initConfirmd / (decimal)Math.Pow(2, i));
+                for (int j = 1; j <= Math.Pow(2, i); j++)
+                {
+                    var index = random.Next(0, address.Length);
+                    var index2 = random.Next(0, address.Length);
+                    if (Tools.Send(address[index], value, address[index2]))
+                    {
+                        Console.WriteLine($"{i} 转账成功：{address[index]} {value}\t{j}");
+                    }
+                }
+                confirmd = Tools.GetBalance();
+                while (confirmd < initConfirmd)
+                {
+                    Thread.Sleep(3000);
+                    confirmd = Tools.GetBalance();
+                    Console.WriteLine($"{i} 已确认{confirmd}/{initConfirmd}");
+                }
+                Console.WriteLine($"第{i}批转账结束");
+            }
+            Console.WriteLine($"测试结束");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AutoSplitCore/Tools.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace AutoSplitCore
+{
+    public static class Tools
+    {
+        public static string HttpPost(string Url, string postData, int timeOut = 60000)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(Url);
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                    request.ContentLength = byteArray.Length;
+                    request.Timeout = timeOut;
+                    Stream dataStream = request.GetRequestStream();
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                    WebResponse response = request.GetResponse();
+                    dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+                    return responseFromServer;
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("网络超时，重试中……");
+                    Console.WriteLine($"Url: {Url}");
+                    continue;
+                }
+            }
+            return "";
+        }
+
+        public static decimal GetBalance()
+        {
+            var json = HttpPost("http://localhost:20332", "{'jsonrpc': '2.0','method': 'getbalance','params': ['c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b'],'id': 1}");
+            return (decimal)JObject.Parse(json)["result"]["confirmed"];
+        }
+
+        public static bool Send(string to, decimal value, string changeAddress = null)
+        {
+            var json = changeAddress == null ?
+            HttpPost("http://localhost:10332", $@"
+            {{
+                'jsonrpc': '2.0',
+                'method': 'sendtoaddress',
+                'params': [
+                    'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
+                    '{to}',
+                    {value}
+                ],
+                'id': 1
+            }}"): HttpPost("http://localhost:10332", $@"
+            {{
+                'jsonrpc': '2.0',
+                'method': 'sendmany',
+                'params': [
+                    [
+                        {{
+                            'asset': 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
+                            'value': {value},
+                            'address': '{to}'
+                        }}
+                    ],
+                    0,
+                    '{changeAddress}'
+                ],
+                'id': 1
+            }}");
+            var a = JObject.Parse(json)["result"];
+            return a != null;
+        }
+    }
+
+}
